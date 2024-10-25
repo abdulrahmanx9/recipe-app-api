47d20bd5ed3f3254a271e868c773d2378fcec715
abdulrahmanx9 <UG_31107079@ics.tanta.edu.eg>
2024-10-26 00:35:02 +0300

    Added GitHub Actions

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,7 +1,22 @@
+import time
+
+from psycopg2 import OpertionalError as Psycopg2OpError
+
+from django.db.utils import OperationalError 
 from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
     
     def handle(self, *args, **options):
-        pass
-    
+        """Entry Point For Command"""
+        self.stdout.write('Writing for database...')
+        db_up = False
+        while db_up is False :
+            try:
+                self.check(databases=['default']) 
+                db_up = True
+            except (Psycopg2OpError, OperationalError):
+                self.stdout.write('Databases unavailable, waiting 1 second...')
+                time.sleep(1)
+        self.stdout.write(self.style.SUCCESS('Databases available!'))
+        
