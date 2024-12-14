3c23848a70fff1821a6251770c1f2ca20d67302c
abdulrahmanx9 <UG_31107079@ics.tanta.edu.eg>
2024-12-14 07:55:48 +0200

    deploy

--- app/app/settings.py
+++ app/app/settings.py
@@ -134,8 +134,8 @@ USE_TZ = True
 STATIC_URL = "/static/static/"
 MEDIA_URL = "/static/media/"
 
-MEDIA_ROOT = "vol/web/media"
-STATIC_ROOT = "vol/web/static"
+MEDIA_ROOT = "/vol/web/media"
+STATIC_ROOT = "/vol/web/static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
