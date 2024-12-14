c3293b05336e98c6ccc63c88c427fc81da3c5bfd
abdulrahmanx9 <UG_31107079@ics.tanta.edu.eg>
2024-12-14 09:56:32 +0200

    update

--- app/app/urls.py
+++ app/app/urls.py
@@ -24,8 +24,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from core import views
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/health-check", views.health_check, name='health-check'),
     path("api/schema/", SpectacularAPIView.as_view(), name="api-schema"),
     path(
         "api/docs/",
